6f5dca6784f6439f96236f384d67cd253e41ac20
Simon Anders <sanders@fs.tum.de>
2025-05-08 08:28:36 +0200

    fixed remaining calls to error

--- src/make_wiggle_vector.c
+++ src/make_wiggle_vector.c
@@ -10,11 +10,8 @@ SEXP make_wiggle_vector( SEXP start, SEXP end, SEXP value, SEXP chrlength )
    memset( REAL(res), 0, length(res) * sizeof(double) );
 
    for( i = 0; i < length(start); i++ ) {
-      if( INTEGER(end)[i] < INTEGER(start)[i] ) {
-         char buf[200];
-	 snprintf( buf, 200, "end[%d] < start[%d]", i+1, i+1 );
-         error( buf );
-      }
+      if( INTEGER(end)[i] < INTEGER(start)[i] ) 
+	 error( "end[%d] < start[%d]", i+1, i+1 );
       if( INTEGER(end)[i] > length(res) )
 	 error( "%s", "'chrlength' is too small" );	 
       for( j = INTEGER(start)[i]; j <= INTEGER(end)[i]; j++ )
